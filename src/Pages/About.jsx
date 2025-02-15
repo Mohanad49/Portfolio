9a0902808b886ff8c0caca9848bd808ad814c002
Mohanad <mohanned.3262@gmail.com>
2025-02-16 00:32:00 +0200

    Update link in AboutPage component

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -221,7 +221,7 @@ const AboutPage = () => {
             </p>
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a href="https://drive.google.com/file/d/1v79teMTme0QDlQkLWMq5CQ6RBhVmVKlH/view?usp=sharing" className="w-full lg:w-auto">
+              <a href="https://drive.google.com/file/d/18iarC9qOsxhySEpHf6pj0SIpsD9oUKq_/view?usp=sharing" className="w-full lg:w-auto">
               <button 
                 data-aos="fade-up"
                 data-aos-duration="800"
